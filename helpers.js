3e59e154fb12d159e2917dbb958ca3790c369de5
Ravana tech <85940832+Thisan01@users.noreply.github.com>
2021-11-25 17:18:06 +0530

    🎲Al😇

---
+++ helpers.js
@@ -0,0 +1,22 @@
+/* Copyright (C) 2021 Thisan01
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+Thisan-Asiata
+*/
+
+function successfullMessage(msg) {
+    return "🧚‍♂️ ASIATA*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🧚‍♂️ ASIATA*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "🧚‍♂️ ASIATA*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
